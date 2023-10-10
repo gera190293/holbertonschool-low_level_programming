9fb24063880abcec00125d86db096e3bfd71dfd6
gera190293 <gera190293@gmail.com>
2023-10-10 16:19:04 -0700

    Hello World

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Print Programming is like building a multilingual puzzle
+ *
+ *Return: zero
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
