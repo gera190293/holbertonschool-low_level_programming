41e115351fd19b05e452f803ac0cdfd46d8ecfaf
gera190293 <gera190293@gmail.com>
2023-10-11 09:28:38 -0700

    C - Variables, if, else, while

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - hexadecimal digits
+ *
+ *Return: zero
+ */
+
+int main(void)
+{
+	char n, m;
+
+	for(n = '0'; n <= '9'; n++)
+		putchar(n);
+	for(m = 'a'; m <= 'f'; m++)
+		putchar(m);
+
+	putchar('\n'):
+}
