7b10de5e7310d7ac5890a02816636698eaadc117
gera190293 <gera190293@gmail.com>
2023-10-11 06:32:46 -0700

    C - Variables, if, else, while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,14 +18,14 @@ int main(void)
 	
 	if (n > 0)
 	{
-		printf("n is positive\n");
+		printf("%d is positive\n", n);
 	}
 	if (n == 0)
 	{
-		printf("n is zero\n");
+		printf("%d is zero\n", n);
 	}
 	else
-	printf("n is negative\n");
+	printf("%d is negative\n", n);
 
 	return (0);
 }
