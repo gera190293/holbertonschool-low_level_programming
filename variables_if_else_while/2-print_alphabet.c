e47bf30d45b588875c70a8e83380cb339f2d66d2
gera190293 <gera190293@gmail.com>
2023-10-11 07:41:48 -0700

    C - Variables, if, else, while

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - print all alphathic
+ *
+ *Return: zero
+ *
+ */
+
+int main(void)
+{
+	char i = "a";
+
+	while (i <= "z")
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
+
