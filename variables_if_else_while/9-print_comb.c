ace719c5f8582dde35baceb2521f9e54b6aa81a7
gera190293 <gera190293@gmail.com>
2023-10-11 09:55:27 -0700

    C - Variables, if, else, while

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,9 +8,14 @@ int main(void)
 {
 	int m;
 
-	for (m = 48; m <= 57; ++m)
+	for (m = 48; m <= 57; m++)
 	{
 		putchar(m);
+		if (m == 57)
+		{
+			break
+			putchar('\n');
+		}
 		putchar(44);
 		putchar(32);
 	}
