b2e1006807867743d9578857bd037c68f78498e1
gera190293 <gera190293@gmail.com>
2023-10-12 07:52:04 -0700

    C - Functions, nested loops

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print ten times the alphabet
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char n;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+			_putchar('\n');
+		}
+	}
+}
