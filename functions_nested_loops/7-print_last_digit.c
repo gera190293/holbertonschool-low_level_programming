fc5880961a32fb194066fbec020f64cfbb9aa706
gera190293 <gera190293@gmail.com>
2023-10-12 10:10:19 -0700

    C - Functions, nested loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *print_last_digit - print last digit of an inter
+ *@n: variable
+ *Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
