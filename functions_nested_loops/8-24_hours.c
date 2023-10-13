16b3b264bcb12d635939321d4cab1d6cb754c2d5
gera190293 <gera190293@gmail.com>
2023-10-13 06:42:51 -0700

    C - Functions, nested loops

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *jack_bauer - print the clock
+ *
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = 0;
+	m = 0;
+	while (h < 0)
+	{
+		for (m = 0; m < 59; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
