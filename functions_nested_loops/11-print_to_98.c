8828a04a031f0b9e588942ca41b29ce945babdf7
gera190293 <gera190293@gmail.com>
2023-10-13 08:15:05 -0700

    C - Functions, nested loops

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
+ *print_to_98 - prints from n to 98
+ *@n: variable
  *
  *
  *
@@ -10,10 +10,10 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-		for(; n < 98; n++)
+		for (; n < 98; n++)
 			printf("%d, ", n);
 	else
-		for(; n > 98; n--)
+		for (; n > 98; n--)
 			printf("%d, ", n);
 	printf("98\n");
 }
