1b39d1a6a59700d6405358c650605fa39b20f53c
gera190293 <gera190293@gmail.com>
2023-10-14 14:47:14 -0700

    C - Variables, if, else, while

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - combination of 3
+ *Return: zero
+ */
+int main(void)
+{
+	int m, n, p;
+
+	for (m = 49; m <= 57; m ++)
+	{
+		for (n = m; n <= 57; n++)
+		{
+			for (p = n; <= 57; p++)
+			{
+				putchar(m - 1);
+				putchar(n - 1);
+				putchar(p);
+				if (m <= 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
