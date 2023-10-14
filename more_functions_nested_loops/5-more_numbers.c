18e53e1c603c1cddbcda5bb8f43dfad77a083e17
gera190293 <gera190293@gmail.com>
2023-10-14 10:58:35 -0700

    C - More functions, more nested loops

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *more_numbers - print numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int n;
+	int m = 0;
+
+	while (m <= 10)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			_putchar (n / 10);
+			_putchar (n % 10);
+			_putchar ('\n');
+		}
+		m++;
+	}
+}
