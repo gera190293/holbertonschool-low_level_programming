5defda7ef70429183f3d2393056bb69724ac8ece
gera190293 <gera190293@gmail.com>
2023-10-14 12:28:48 -0700

    C - More functions, more nested loops

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,11 +9,19 @@ void print_diagonal(int n)
 	int i,l;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
+	{
 		for (i = 1; i <= n; i++)
+		{
 			for (l = 1; l <= i; l++)
+			{
 				_putchar(' ');
+			}
 		_putchar("\\");
 		_putchar('\n');
+		}
+	}
 }
