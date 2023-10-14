b80e8cbae96c058857bdb43f327565f5a8bfafb1
gera190293 <gera190293@gmail.com>
2023-10-14 12:25:00 -0700

    C - More functions, more nested loops

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_diagonal - print a diagonal
+ *@n: times repeat diagonal
+ *
+ */
+void print_diagonal(int n)
+{
+	int i,l;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (i = 1; i <= n; i++)
+			for (l = 1; l <= i; l++)
+				_putchar(' ');
+		_putchar("\\");
+		_putchar('\n');
+}
