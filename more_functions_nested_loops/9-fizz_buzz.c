72d5cb7ecee66bcc4ab36a41b577c9d0b77e8097
gera190293 <gera190293@gmail.com>
2023-10-14 13:08:04 -0700

    C - More functions, more nested loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,16 +6,6 @@
  */
 int main(void)
 {
-	return (0);
-}
-
-/**
- *fizz_buzz - print numbers from 1 to 100, but if is multiple of 3 or 5 print Fizz or Buzz
- *
- *
- */
-void fizz_buzz(void)
-{
 	int i = 1;
 
 	while (i <= 100)
@@ -33,4 +23,5 @@ void fizz_buzz(void)
 			printf("%d", i);
 		}
 	}
+	return (0);
 }
