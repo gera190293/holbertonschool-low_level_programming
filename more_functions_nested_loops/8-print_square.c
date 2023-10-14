6002877b861ce7facf09e2abff68c0932cfb45e5
gera190293 <gera190293@gmail.com>
2023-10-14 12:44:14 -0700

    C - More functions, more nested loops

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,10 +9,18 @@ void print_square(int size)
 	int i, l;
 
 	if (size <= 0)
+	{
 		_putchar(10);
+	}
 	else
+	{
 		for (i = 0; i <= size; i++)
+		{
 			for (l = 0; l <= size; l++)
+			{
 				_putchar(35);
+			}
 		_putchar(10);
+		}
+	}
 }
