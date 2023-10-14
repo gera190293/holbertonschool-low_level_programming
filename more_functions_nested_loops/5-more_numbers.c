ac98b567c28bbfa9ee4be420f9b060eb66e78465
gera190293 <gera190293@gmail.com>
2023-10-14 11:19:36 -0700

    C - More functions, more nested loops

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,7 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n / 10 == 0)
-				continue;
-			else
-				_putchar (n / 10 + '0');
+			_putchar (n / 10 + '0');
 			_putchar (n % 10 + '0');
 		}
 		_putchar ('\n');
