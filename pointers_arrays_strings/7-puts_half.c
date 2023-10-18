f375d1db9572ce0b76e0eb85ad1e537dfd684b89
gera190293 <gera190293@gmail.com>
2023-10-18 09:53:38 -0700

    C - Pointers, arrays and strings

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,10 +14,16 @@ void puts_half(char *str)
 	while (str[i] != '\0')
 		i++;
 	l = i / 2;
+	if (i % 2 != 0)
+	{
+		l = l + 1;
+	}
+	else
+		l = l;
 	while (str[l] != '\0')
 	{
-		l++;
 		_putchar(str[l]);
+		l++;
 	}
 	_putchar(10);
 }
