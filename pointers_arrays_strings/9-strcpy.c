f4b761919bfadaf54bc422463250723b8d7e026a
gera190293 <gera190293@gmail.com>
2023-10-18 13:51:05 -0700

    C - Pointers, arrays and strings

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,11 +7,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src != '\0')
+	int i = 0;
+
+	while (src[i] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
