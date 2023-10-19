d704d2087f0195be2b9fa3f7afddbfbab3745013
gera190293 <gera190293@gmail.com>
2023-10-19 10:29:46 -0700

    C - More pointers, arrays and strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strcat - concatenate two strings
+ *@dest: destination variable
+ *@src: string to concatenate
+ *Return: the result
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
