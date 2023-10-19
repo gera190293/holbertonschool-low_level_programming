6631fe17ef83c11c4261fc7789e996882661c8ed
gera190293 <gera190293@gmail.com>
2023-10-19 10:46:34 -0700

    C - More pointers, arrays and strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strncnat - concatenate n bytes of the string
+ *@dest: destination
+ *@src: source
+ *@n: bytes
+ *Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int = 0;
+	int = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (j <= n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (*dest);
+}
