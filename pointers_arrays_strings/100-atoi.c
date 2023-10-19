ffea8b8e3ee374b9d90e95dae815f7f1e1085f22
gera190293 <gera190293@gmail.com>
2023-10-19 09:14:20 -0700

    C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_atoi - convert char into int
+ *@s: string
+ *Return: string in int
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+
+	while (i != '\0')
+	{
+		if (s[i] == '-')
+		{
+			s[i] = -s[i];
+			i++;
+		}
+		else if (s[i] <= 9 && s[i] >= 0)
+		{
+			s[i] = s[i];
+			i++;
+		}
+		else
+			continue;
+	}
+}
