16bcede1f8d292a5d404842bd42511c585bb62de
gera190293 <gera190293@gmail.com>
2023-10-20 08:12:17 -0700

    C - More pointers, arrays and strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcmp - compare two strings
+ *@s1: variable 1
+ *@s2: variable 2
+ *Return: 0 equal, 1 if s1 is greater than s2, -1 else
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else
+			i++;
+	}
+	return (s1[i] - s2[i]);
+}
