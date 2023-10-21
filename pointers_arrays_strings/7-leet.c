0dcbd63f656a0562907ac401ef8f0d3d54f3220f
gera190293 <gera190293@gmail.com>
2023-10-20 17:40:05 -0700

    C - More pointers, arrays and strings

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main,h"
+/**
+ *leet - encode intro 1337
+ *@s: string
+ *Return: new string
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char s2[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (s[i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (s[j] == s2[j] || s[i] - 32 == s2[j])
+				s[i] = j + '0';
+		}
+		i++;
+	}
+	return (s);
+}
