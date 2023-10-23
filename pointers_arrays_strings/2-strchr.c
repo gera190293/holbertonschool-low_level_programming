29b0f44daac347b005ebe65c666e3e6aa196635b
gera190293 <gera190293@gmail.com>
2023-10-23 07:10:04 -0700

    C - Even more pointers, arrays and strings

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strchr - search for a character in the string
+ *@s: the string
+ *@c: the character
+ *Return: pointer to the first occurrence of the character 
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] != c)
+			i++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+}
