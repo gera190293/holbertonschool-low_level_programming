57b355efe858ee6f7ed560612a8be4e900f4e1dd
gera190293 <gera190293@gmail.com>
2023-10-24 11:41:06 -0700

    Recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts_recursion - puts in recursion
+ *@s: string
+ *
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+		_putchar('\n');
+	_putchar(*s)
+	_puts_recursion(*s + 1);
+}
