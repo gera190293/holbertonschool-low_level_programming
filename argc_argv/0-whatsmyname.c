f0c6b4efd74dd322dfd9ea9f9bc85aaafe159f54
gera190293 <gera190293@gmail.com>
2023-10-31 17:20:11 -0700

    Argc and Argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  *main - Write a program that prints its name, followed by a new line.
  *@argc: number of arguments
  *@argv: pointer to arguments
- *return: zero
+ *Return: zero
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
