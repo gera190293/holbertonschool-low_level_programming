49ab57973c4c5b237da2f1a0963273fd49917122
gera190293 <gera190293@gmail.com>
2023-10-31 17:27:58 -0700

    Argc and Argv

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - print every argument
+ *@argc: count the arguments
+ *@argv: pointer to arguments
+ *
+ *Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+		printf("%s\n", argv [i];
+	return (0);
+}
