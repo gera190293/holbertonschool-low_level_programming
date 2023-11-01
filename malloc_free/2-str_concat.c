17d37f3b387347a95d8ee2c4a8f781a5fa044784
gera190293 <gera190293@gmail.com>
2023-11-01 15:24:52 -0700

    Malloc and Free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,10 +11,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s3;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	s3 = malloc(sizeof(*s3) * (strlen(s1) + strlen(s2) + 1));
 	if (s3 == NULL)
 		return (NULL);
-	strcat(s1, s2);
-	strcpy(s3, s1);
+	s3 = strcat(s1, s2);
 	return (s3);
 }
