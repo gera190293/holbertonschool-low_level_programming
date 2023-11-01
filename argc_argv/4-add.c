af1fafff7f21f870cb8ca65b8ae2e180d670445b
gera190293 <gera190293@gmail.com>
2023-10-31 18:12:57 -0700

    Argc and Argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,11 +12,11 @@ int main(int argc, char *argv[])
 {
 	int i, j, res = 0;
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '0')
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (-1);
