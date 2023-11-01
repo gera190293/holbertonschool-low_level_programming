e84de314edc6f0f8de05ecd9910783e15a8140ba
gera190293 <gera190293@gmail.com>
2023-11-01 09:32:44 -0700

    Malloc and Free

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,9 +9,12 @@
  */
 char *_strdup(char *str)
 {
+	if (str == NULL)
+		return (NULL);
+
 	char *str2 = malloc(sizeof(*str2) * (strlen(str) + 1));
 
-	if (str2 == NULL || str == NULL)
+	if (str2 == NULL)
 		return (NULL);
 	strcpy(str2, str);
 	return (str2);
