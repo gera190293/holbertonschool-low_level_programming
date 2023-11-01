7be1c23336b5c8f88b9373386606afbd97b79bbd
gera190293 <gera190293@gmail.com>
2023-10-31 17:12:09 -0700

    Argc and Argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *main - Write a program that prints its name, followed by a new line.
+ *@argc: number of arguments
+ *@argv: pointer to arguments
+ *
+ */
+void main(int argc, char *argv[])
+{
+	printf("%s", argv[0])
+}
