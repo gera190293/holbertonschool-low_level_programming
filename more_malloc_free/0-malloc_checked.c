3d5b883f96ec902b1a114e8e4ddac78075e0072e
gera190293 <gera190293@gmail.com>
2023-11-02 07:42:58 -0700

    More Malloc and Free

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,15 @@
 /**
  *malloc_checked - function that allocates memory using malloc
  *@b: variable
- *Return: if malloc fails, the malloc_checked function should 
+ *Return: if malloc fails, the malloc_checked function should
  *cause normal process termination with a status value of 98
  *
  */
 void *malloc_checked(unsigned int b)
 {
 	void *m = malloc(b);
+
 	if (m == NULL)
 		exit(98);
-	return(m);
+	return (m);
 }
