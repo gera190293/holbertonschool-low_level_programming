ba03d50166bdecbafb344e7ee0fdc2ccacbdd8db
gera190293 <gera190293@gmail.com>
2023-11-02 10:05:24 -0700

    Preprocessors

--- preprocessor/1-pi.h
+++ preprocessor/1-pi.h
@@ -1,10 +1,8 @@
 #ifndef PI_H
 #define PI_H
 /**
- *
- *Create a header file that defines a macro named PI as
+ *Description: Create a header file that defines a macro named PI as
  *an abbreviation for the token 3.14159265359.
- *
  */
 #define PI 3.14159265359
 
