c708f22b49838d3d91fceb908e9d9daa8b3b0bf3
gera190293 <gera190293@gmail.com>
2023-11-10 10:48:58 -0800

    Function pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
-	for (;i < size; i++)
+	for (; i < size; i++)
 		if (cmp(array[i]) != 0)
 			return (i);
 	return (-1);
