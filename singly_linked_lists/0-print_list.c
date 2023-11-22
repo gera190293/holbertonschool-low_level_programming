e9837b42e225b801c26ad2f016c58685d114837e
gera190293 <gera190293@gmail.com>
2023-11-22 05:33:39 -0800

    singled linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *print_list - function that prints all the elements of a list_t list
+ *@h: single list
+ *Return: number of nodes
+ *
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	if (str == NULL)
+		printf([0] (nil));	
