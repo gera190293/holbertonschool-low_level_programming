477958356f8c0bd4fdf19a1bf35f9d3e93d8fded
gera190293 <gera190293@gmail.com>
2023-11-24 01:39:36 -0300

    Single linked list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,12 +12,13 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)");
 			count++;
+			h = h->next;
 		}
 		printf("[%d] %s\n", h->len, h->str);
 		count++;
