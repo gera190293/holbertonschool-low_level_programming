4c46aad328c4f849683116b0a30b86ceeea753e4
gera190293 <gera190293@gmail.com>
2023-12-01 14:27:46 -0800

    C - Bit manipulation

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int get_bit(unsigned long int n, unsigned int index);
+#include "2-get_bit.c"
 /**
  *set_bit - function that sets the value of a bit to 1 at a given index.
  *@n: pointer to n
