39cb3c75235c17dd5298fb6274b56f631a48b8f3
gera190293 <135637635+gera190293@users.noreply.github.com>
2023-12-04 17:33:01 -0300

    Pre_shell

---
+++ Pre_shell/pre_shell.h
@@ -0,0 +1,10 @@
+#ifndef PRE_SHELL_H
+#define PRE_SHELL_H
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void print_PID(void);
+
+#endif
---
+++ Pre_shell/print_PID.c
@@ -0,0 +1,14 @@
+#include "pre_shell.h"
+/**
+* print_PID - print the ID of the parent process
+* 
+* 
+* 
+*/
+void print_PID(void)
+{
+    pid_t pid;
+
+    pid = getpid();
+    printf("%d", pid);
+}
