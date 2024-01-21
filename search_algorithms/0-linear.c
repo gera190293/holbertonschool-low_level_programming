7fba50a433be9319f86196e37833b36db32248e0
gera190293 <gera190293@gmail.com>
2024-01-21 09:55:57 -0800

    C - Search Algorithms

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- *linear_search - function that searches for a value in an array of integers 
+ *linear_search - function that searches for a value in an array of integers
  * using the Linear search algorithm
  *@array: pointer to the first element of the array to search in
  *@size: is the number of elements in array
